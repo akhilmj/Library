3322a354af441bd46c8c7442ff8291c23f4ed4a1
Akhil m j <akhilmjoy8@gmail.com>
2020-08-15 21:17:06 +0530

    test

--- Library/Library.Data/Migrations/20200815152404_Book.Designer.cs
+++ Library/Library.Data/Migrations/20200815152404_Book.Designer.cs
@@ -8,13 +8,46 @@ using Library.Data;
 namespace Library.Data.Migrations
 {
     [DbContext(typeof(LibraryContext))]
-    partial class LibraryContextModelSnapshot : ModelSnapshot
+    [Migration("20200815152404_Book")]
+    partial class Book
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "1.1.3")
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("Library.Data.Models.Books", b =>
+                {
+                    b.Property<int>("BookID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("AutherName");
+
+                    b.Property<string>("BookName");
+
+                    b.Property<int?>("CreatedBy");
+
+                    b.Property<DateTime?>("CreatedDate");
+
+                    b.Property<short?>("IsActive");
+
+                    b.Property<DateTime?>("LastUpdatedDate");
+
+                    b.Property<DateTime?>("PublishDate");
+
+                    b.Property<string>("PublisherName");
+
+                    b.Property<int?>("UpdatedBy");
+
+                    b.HasKey("BookID");
+
+                    b.HasIndex("CreatedBy");
+
+                    b.HasIndex("UpdatedBy");
+
+                    b.ToTable("Books");
+                });
 
             modelBuilder.Entity("Library.Data.Models.Users", b =>
                 {
@@ -44,6 +77,17 @@ namespace Library.Data.Migrations
                     b.HasIndex("UpdatedBy");
 
                     b.ToTable("Users");
+                });
+
+            modelBuilder.Entity("Library.Data.Models.Books", b =>
+                {
+                    b.HasOne("Library.Data.Models.Users", "user1")
+                        .WithMany()
+                        .HasForeignKey("CreatedBy");
+
+                    b.HasOne("Library.Data.Models.Users", "user2")
+                        .WithMany()
+                        .HasForeignKey("UpdatedBy");
                 });
 
             modelBuilder.Entity("Library.Data.Models.Users", b =>
